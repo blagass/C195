c1efd4a43f377c870ec364fbe43d6ecb46b68ed9
Brandon <brandon.lagasse@gmail.com>
2023-04-20 01:50:03 -0700

    Graphical update - Customers/Appointment screen seperation

--- src/main/java/brandonlagasse/c195/MainScreen.java
+++ src/main/java/brandonlagasse/c195/MainScreen.java
@@ -11,8 +11,11 @@ public class MainScreen extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainScreen.class.getResource("main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 1400, 700);
+        String css = this.getClass().getResource("style.css").toExternalForm();
+        scene.getStylesheets().add(css);
         stage.setTitle("Hello!");
+        stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
     }
